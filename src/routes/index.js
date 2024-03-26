c9d865ff3725115abf0a75fd6967742e4830a7fa
princoo <princeineza@gmail.com>
2024-03-26 17:35:44 +0200

    setting database

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+// eslint-disable-next-line no-unused-vars
+import sequelize from '../database/config/db';
+
+const routes = express.Router();
+
+routes.get('/', () => {});
+
+export default routes;
