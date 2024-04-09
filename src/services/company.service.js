62454c661c1baddbf6217a6016034efbabeba293
Pacific Nduwumwe <58509941+Pacific911@users.noreply.github.com>
2024-04-09 16:39:28 -0700

    added all the operations of the company

--- src/services/company.service.js
+++ src/services/company.service.js
@@ -1,4 +1,5 @@
 import Companies from '../database/models/company';
+import Events from '../database/models/events';
 
 async function createCompany(details) {
   const user = await Companies.create(details);
@@ -9,7 +10,49 @@ async function getCompanyByName(name) {
   return user;
 }
 async function getCompanyById(id) {
-  const user = await Companies.findOne({ where: { id } });
+  const user = await Companies.findOne({
+    where: { id },
+    include: [
+      {
+        model: Events,
+        as: 'Events',
+      },
+    ],
+  });
   return user;
 }
-export default { createCompany, getCompanyByName, getCompanyById };
+async function deleteCompany(id) {
+  const user = await Companies.destroy({ where: { id } });
+  return user;
+}
+async function getAllCompanies() {
+  const user = await Companies.findAll({
+    include: [
+      {
+        model: Events,
+        as: 'Events',
+      },
+    ],
+  });
+  return user;
+}
+async function updateCompany(id, details) {
+  try {
+    const company = await Companies.findByPk(id);
+    if (!company) {
+      throw new Error('Company not found');
+    }
+    await company.update(details);
+    return company;
+  } catch (error) {
+    throw new Error(`Error updating company: ${error.message}`);
+  }
+}
+export default {
+  createCompany,
+  getCompanyByName,
+  getCompanyById,
+  deleteCompany,
+  getAllCompanies,
+  updateCompany,
+};
